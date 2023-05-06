e2dee79b8b9b41b410b5aa23f653ad67431721e4
Raymond Chen <ahrchen@gmail.com>
2023-05-06 16:06:18 -0700

    Initial Commit

---
+++ Set/SetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetApp.swift
+//  Set
+//
+//  Created by Raymond Chen on 5/6/23.
+//
+
+import SwiftUI
+
+@main
+struct SetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
